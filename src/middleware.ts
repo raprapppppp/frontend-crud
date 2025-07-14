ce3a6338b19cf2d19dac1d11e80ceeb892dbf80b
rap <cpe.marquez.rafael@gmail.com>
2025-07-14 21:07:30 +0800

    Fixing middleware and layout - Adding pretteir settings

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+const protectedRoutes = ["/dashboard"]
+
+export default function middleware(request: NextRequest) {
+	console.log("test")
+	// Check if the current path is a protected route
+	const isProtectedRoute = protectedRoutes.some((route) =>
+		request.nextUrl.pathname.startsWith(route)
+	)
+
+	if (isProtectedRoute) {
+		const sessionCookie = request.cookies.get("token")
+		if (!sessionCookie) {
+			return NextResponse.redirect(new URL("/login", request.url))
+		}
+		return NextResponse.next()
+	}
+}
+
+export const config = {
+	matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)", "/dashboard"], // Match protected routes
+}
